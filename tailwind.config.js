6731b4e229f92c448cc990cf18db08de46f9421b
Andrew Guo <andrew.c.guo@gmail.com>
2022-11-27 23:19:30 -0500

    tailwind config + added npm supabase

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkPrimary: '#44475a',
+        darkSecondary: '#282a36',
+        darkText: '#f8f8f2',
+        draculaPink: 'ff79c6',
+        draculaPurple: 'bd93f9',
+        draculaCyan: '8be9fd',
+        draculaYellow: 'f1fa8c',
+        draculaGreen: '50fa7b',
+        draculaRed: 'ff5555',
+        lightPrimary: '#1f3855',
+        lightSecondary: '#f7f2ea', 
+        lightText: '#414756' 
+      }
+    },
   },
   plugins: [],
 }
