e5dc7a2a031d0e6ff95d03e7fe819c5b4272dbc0
Andrew Guo <andrew.c.guo@gmail.com>
2022-11-28 02:33:30 -0500

    done login pages

--- pages/owner/login.tsx
+++ pages/owner/login.tsx
@@ -9,20 +9,24 @@ const Home = () => {
   return (
     <Layout title="Login | Look-Inna-Book">
       <div className="flex flex-wrap flex-col items-center justify-around sm:my-16 sm:flex-row sm:items-start sm:justify-center">
-        <div className="w-1/3">
-        <h1>Look-Inna-Book</h1>
-          {!session ? (
-            <Auth
-              providers={["github", "google", "linkedin"]}
-              socialLayout="horizontal"
-              supabaseClient={supabase}
-              appearance={{ theme: ThemeSupa }}
-              magicLink
-              theme="dark"
-            />
-          ) : (
-            <p>Account page will go here.</p>
-          )}
+        <div className="lg:w-2/5 sm:w-1/2">
+          <div className="bg-neutral-800 pt-12 px-12 pb-10 rounded-3xl shadow-xl">
+            <h1 className="text-draculaYellow text-3xl pb-4 text-center">
+              Owner Login
+            </h1>
+            {!session ? (
+              <Auth
+                providers={["github", "google", "linkedin"]}
+                socialLayout="horizontal"
+                supabaseClient={supabase}
+                appearance={{ theme: ThemeSupa }}
+                showLinks={false}
+                theme="dark"
+              />
+            ) : (
+              <p>Account page will go here.</p>
+            )}
+          </div>
         </div>
       </div>
     </Layout>
