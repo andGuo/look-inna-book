09919f0aa28a83c4093047a7eeb0f6cf7ad60fab
Andrew Guo <andrew.c.guo@gmail.com>
2022-11-28 17:14:43 -0500

    querying profile fname and last name kinda works

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,77 +1,91 @@
-import { useState, useEffect } from 'react'
-import { useUser, useSupabaseClient, Session } from '@supabase/auth-helpers-react'
-import { Database } from '../utils/database.types'
-type Profiles = Database['public']['Tables']['profiles']['Row']
+import { useState, useEffect } from "react";
+import {
+  useUser,
+  useSupabaseClient,
+  Session,
+} from "@supabase/auth-helpers-react";
+import { Database } from "../utils/database.types";
+import { count } from "console";
+type Profiles = Database["public"]["Tables"]["profiles"]["Row"];
+type Address = Database["public"]["Tables"]["user_address"]["Row"];
 
-export default function Account({ session }: { session: Session }) {
-  const supabase = useSupabaseClient<Database>()
-  const user = useUser()
-  const [loading, setLoading] = useState(true)
-  const [username, setUsername] = useState<Profiles['username']>(null)
-  const [website, setWebsite] = useState<Profiles['website']>(null)
-  const [avatar_url, setAvatarUrl] = useState<Profiles['avatar_url']>(null)
+export default function Profile({ session }: { session: Session }) {
+  const supabase = useSupabaseClient<Database>();
+  const user = useUser();
+  const [loading, setLoading] = useState(true);
+  const [fname, setFname] = useState<Profiles["first_name"]>(null);
+  const [lname, setLname] = useState<Profiles["last_name"]>(null);
+  const [addr, setAddr] = useState<Address["address"]>("");
+  const [aptSuite, setAptSuite] = useState<Address["apartment_suite"]>(null);
+  const [country, setCountry] = useState<Address["country"]>("");
+  const [city, setCity] = useState<Address["city"]>("");
+  const [state, setState] = useState<Address["state"]>("");
+  const [zipCode, setZipCode] = useState<Address["zip_code"]>("");
+  const [phoneNum, setPhoneNum] = useState<Address["phone_number"]>("");
 
   useEffect(() => {
-    getProfile()
-  }, [session])
+    getProfile();
+  }, [session]);
 
   async function getProfile() {
     try {
-      setLoading(true)
-      if (!user) throw new Error('No user')
+      setLoading(true);
+      if (!user) throw new Error("No user");
 
       let { data, error, status } = await supabase
-        .from('profiles')
-        .select(`username, website, avatar_url`)
-        .eq('id', user.id)
-        .single()
+        .from("profiles")
+        .select(`first_name, last_name`)
+        .eq("profile_id", user.id)
+        .single();
 
       if (error && status !== 406) {
-        throw error
+        throw error;
       }
 
       if (data) {
-        setUsername(data.username)
-        setWebsite(data.website)
-        setAvatarUrl(data.avatar_url)
+        setFname(data.first_name);
+        setLname(data.last_name);
       }
     } catch (error) {
-      alert('Error loading user data!')
-      console.log(error)
+      alert("Error loading user data!");
+      console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
   async function updateProfile({
-    username,
-    website,
-    avatar_url,
+    first_name, last_name, address, apartment_suite, country, city, state, zip_code, phone_number
   }: {
-    username: Profiles['username']
-    website: Profiles['website']
-    avatar_url: Profiles['avatar_url']
+    first_name: Profiles["first_name"];
+    last_name: Profiles["last_name"];
+    address: Address["address"];
+    apartment_suite: Address["apartment_suite"];
+    country: Address["country"];
+    city: Address["city"];
+    state: Address["state"];
+    zip_code: Address["address"];
+    phone_number: Address["phone_number"];
   }) {
     try {
-      setLoading(true)
-      if (!user) throw new Error('No user')
+      setLoading(true);
+      if (!user) throw new Error("No user");
+      if (!fname || !lname) throw new Error("No fname or lname");
 
       const updates = {
-        id: user.id,
-        username,
-        website,
-        avatar_url,
-        updated_at: new Date().toISOString(),
-      }
+        profile_id: user.id,
+        first_name: fname,
+        last_name: lname,
+      };
 
-      let { error } = await supabase.from('profiles').upsert(updates)
-      if (error) throw error
-      alert('Profile updated!')
+      let { error } = await supabase.from("profiles").update(updates);
+      if (error) throw error;
+      alert("Profile updated!");
     } catch (error) {
-      alert('Error updating the data!')
-      console.log(error)
+      alert("Error updating the data!");
+      console.log(error);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
@@ -82,39 +96,104 @@ export default function Account({ session }: { session: Session }) {
         <input id="email" type="text" value={session.user.email} disabled />
       </div>
       <div>
-        <label htmlFor="username">Username</label>
+        <label htmlFor="fname">First Name</label>
         <input
-          id="username"
+          id="fname"
           type="text"
-          value={username || ''}
-          onChange={(e) => setUsername(e.target.value)}
+          value={fname || ""}
+          onChange={(e) => setFname(e.target.value)}
         />
       </div>
       <div>
-        <label htmlFor="website">Website</label>
+        <label htmlFor="lname">Last Name</label>
         <input
-          id="website"
-          type="website"
-          value={website || ''}
-          onChange={(e) => setWebsite(e.target.value)}
+          id="lname"
+          type="text"
+          value={lname || ""}
+          onChange={(e) => setLname(e.target.value)}
         />
+      </div>
+      <div>
+        <label htmlFor="address">Address</label>
+        <input
+          id="address"
+          type="text"
+          value={addr || ""}
+          onChange={(e) => setAddr(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="apartment_suite">Apartment/Suite</label>
+        <input
+          id="apartment_suite"
+          type="text"
+          value={aptSuite || ""}
+          onChange={(e) => setAptSuite(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="country">Country</label>
+        <input
+          id="country"
+          type="text"
+          value={country || ""}
+          onChange={(e) => setCountry(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="city">Last Name</label>
+        <input
+          id="city"
+          type="text"
+          value={city || ""}
+          onChange={(e) => setCity(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="state">State/Province</label>
+        <input
+          id="state"
+          type="text"
+          value={state || ""}
+          onChange={(e) => setState(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="zip_code">Postal Code</label>
+        <input
+          id="zip_code"
+          type="text"
+          value={zipCode || ""}
+          onChange={(e) => setZipCode(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="phone_number">Phone Number</label>
+        <input
+          id="phone_number"
+          type="text"
+          value={phoneNum || ""}
+          onChange={(e) => setPhoneNum(e.target.value)}
+        />
+      </div>
+      <div>
+        <button
+          className="button primary block"
+          onClick={() => updateProfile({ fname, lname, addr, aptSuite, country, city, state, zipCode, phoneNum })}
+          disabled={loading}
+        >
+          {loading ? "Loading ..." : "Update"}
+        </button>
       </div>
 
       <div>
         <button
-          className="button primary block"
-          onClick={() => updateProfile({ username, website, avatar_url })}
-          disabled={loading}
+          className="button block"
+          onClick={() => supabase.auth.signOut()}
         >
-          {loading ? 'Loading ...' : 'Update'}
-        </button>
-      </div>
-
-      <div>
-        <button className="button block" onClick={() => supabase.auth.signOut()}>
           Sign Out
         </button>
       </div>
     </div>
-  )
+  );
 }
--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,6 +1,7 @@
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Layout from "../../components/Layout";
+import Profile from "../../components/Profile";
 
 const Home = () => {
   const session = useSession();
@@ -11,21 +12,28 @@ const Home = () => {
       <div className="flex flex-wrap flex-col items-center justify-around sm:my-14 sm:flex-row sm:items-start sm:justify-center">
         <div className="lg:w-2/5 sm:w-1/2">
           <div className="bg-neutral-800 pt-12 px-12 pb-6 rounded-3xl shadow-xl">
-            <h1 className="text-draculaGreen text-3xl pb-4 text-center">
-              User Login
-            </h1>
             {!session ? (
-              <Auth
-                /*providers={["github", "google", "linkedin"]}*/
-                socialLayout="horizontal"
-                supabaseClient={supabase}
-                appearance={{ theme: ThemeSupa }}
-                magicLink={true}
-                redirectTo={'${host}'}
-                theme="dark"
-              />
+              <div>
+                <h1 className="text-draculaGreen text-3xl pb-4 text-center">
+                  User Login
+                </h1>
+                <Auth
+                  /*providers={["github", "google", "linkedin"]}*/
+                  socialLayout="horizontal"
+                  supabaseClient={supabase}
+                  appearance={{ theme: ThemeSupa }}
+                  magicLink={true}
+                  redirectTo={"${host}"}
+                  theme="dark"
+                />
+              </div>
             ) : (
-              <p>Account page will go here.</p>
+              <div>
+                <h1 className="text-draculaGreen text-3xl pb-4 text-center">
+                  User Info
+                </h1>
+                <Profile session={session} />
+              </div>
             )}
           </div>
         </div>
