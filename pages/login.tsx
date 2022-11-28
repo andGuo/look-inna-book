e815c846bf0d244cc6d1e4bd8612af9da16835de
Andrew Guo <andrew.c.guo@gmail.com>
2022-11-28 01:33:37 -0500

    Layout component and login pages kinda work

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,32 @@
-import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import Layout from "../components/Layout";
 
 const Home = () => {
-  const session = useSession()
-  const supabase = useSupabaseClient()
+  const session = useSession();
+  const supabase = useSupabaseClient();
 
   return (
-    <div className="container" style={{ padding: '50px 0 100px 0' }}>
-      {!session ? (
-        <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
-      ) : (
-        <p>Account page will go here.</p>
-      )}
-    </div>
-  )
-}
+    <Layout title="Login | Look-Inna-Book">
+      <div className="flex flex-wrap flex-col items-center justify-around sm:my-16 sm:flex-row sm:items-start sm:justify-center">
+        <div className="w-1/3">
+        <h1>Look-Inna-Book</h1>
+          {!session ? (
+            <Auth
+              providers={["github", "google", "linkedin"]}
+              socialLayout="horizontal"
+              supabaseClient={supabase}
+              appearance={{ theme: ThemeSupa }}
+              magicLink
+              theme="dark"
+            />
+          ) : (
+            <p>Account page will go here.</p>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default Home
+export default Home;
