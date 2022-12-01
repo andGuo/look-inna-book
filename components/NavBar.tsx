b62424ef5e9368410ee24fffa334ad7dbe988e69
Andrew Guo <andrew.c.guo@gmail.com>
2022-12-01 14:11:02 -0500

    Made links and cart svg

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
   return (
     <nav className="flex flex-wrap items-center justify-between content-end pt-5">
       <div className="flex items-center logo h-12 px-6 py-3 text-5xl font-bold">
-        <Link className="text-darkText hover:text-draculaCyan" href="/">
+        <Link className="text-draculaGreen hover:text-draculaCyan" href="/">
           Look-Inna-Book
         </Link>
       </div>
@@ -34,7 +34,16 @@ const NavBar = () => {
           <li className="navBtn">
             <Link href="/owner">Owner Page</Link>
           </li>
-          <li className="navBtn">
+          <li className="navBtn flex">
+            <svg
+              aria-hidden="true"
+              className="mr-2 -ml-1 w-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
+            </svg>
             <Link href="/checkout">Cart</Link>
           </li>
         </ul>
---
+++ pages/owner/management.tsx
@@ -0,0 +1,45 @@
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
+import Link from "next/link";
+import Layout from "../../components/Layout";
+
+const Home = () => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
+  return (
+    <Layout title="Login | Look-Inna-Book">
+      <div className="flex flex-wrap flex-col items-center justify-around sm:my-14 sm:flex-row sm:items-start sm:justify-center">
+        <div className="lg:w-2/5 sm:w-1/2">
+          <div className="bg-neutral-800 pt-12 px-12 pb-6 rounded-3xl shadow-xl">
+            <h1 className="text-draculaCyan text-3xl pb-4 text-center">
+              Owner Management Page
+            </h1>
+            <ul>
+              <li className="navBtn">
+                <Link href="/add/book">Add New Book</Link>
+              </li>
+              <li className="navBtn">
+                <Link href="/remove/book">Remove Books</Link>
+              </li>
+              <li className="navBtn">
+                <Link href="/add/author">Add Author</Link>
+              </li>
+              <li className="navBtn">
+                <Link href="/add/publisher">Add Publisher</Link>
+              </li>
+              <li className="navBtn">
+                <Link href="/add/genre">Add Genre</Link>
+              </li>
+              <li className="navBtn">
+                <Link href="/report">Generate Report</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
