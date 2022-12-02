11580681c936e547a59e39c50e0a308bc8e79cde
Andrew Guo <andrew.c.guo@gmail.com>
2022-12-01 23:47:31 -0500

    made sorry page and done middleware

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,43 @@
-import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
   // We need to create a response and hand it to the supabase client to be able to modify the response headers.
-  const res = NextResponse.next()
+  const res = NextResponse.next();
   // Create authenticated Supabase Client.
-  const supabase = createMiddlewareSupabaseClient({ req, res })
+  const supabase = createMiddlewareSupabaseClient({ req, res });
   // Check if we have a session
   const {
     data: { session },
-  } = await supabase.auth.getSession()
+  } = await supabase.auth.getSession();
 
-  if (session){
-    let { data, error, status } = await supabase.rpc("isOwner", session.user.id);
-    if (false){
+  // Not authenticated
+  if (!session) {
+    const redirectUrl = req.nextUrl.clone();
+    redirectUrl.pathname = "/user";
+    redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (session) {
+    let { data, error, status } = await supabase.rpc(
+      "isOwner",
+      session.user.id
+    );
+    if (false) {
       // Authentication successful, forward request to protected route.
-      return res
+      return res;
     }
   }
 
   // Auth condition not met, redirect to home page.
-  const redirectUrl = req.nextUrl.clone()
-  redirectUrl.pathname = '/user'
-  redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname)
-  return NextResponse.redirect(redirectUrl)
+  const redirectUrl = req.nextUrl.clone();
+  redirectUrl.pathname = "/sorry";
+  redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
+  return NextResponse.redirect(redirectUrl);
 }
 
 export const config = {
-  matcher: ['/owner', '/owner/:path*'],
-}
+  matcher: ["/owner", "/owner/:path*"],
+};
---
+++ pages/sorry.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const Sorry = () => {
+  return (
+    <Layout title="Login | Look-Inna-Book">
+      <div className="flex flex-wrap flex-col items-center justify-around sm:my-14 sm:flex-row sm:items-start sm:justify-center">
+        <div className="lg:w-2/5 sm:w-1/2">
+          <div className="bg-neutral-800 pt-12 px-12 pb-6 rounded-3xl shadow-xl flex flex-col justify-center items-center">
+            <h1 className="text-6xl mb-4 text-draculaRed">Sorry!</h1>
+            <h2 className="text-3xl mb-4 text-darkText text-center">
+              You do not have permission to view that page. Contact andGuo if this shouldn't be the case.
+            </h2>
+            <button className="bg-draculaRed hover:bg-draculaPink text-white font-bold py-2 px-4 border-b-4 border-draculaPink hover:border-draculaGreen rounded">
+              <Link className="text-3xl" href="/">
+                Return Home
+              </Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Sorry;
