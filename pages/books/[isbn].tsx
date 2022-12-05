88cfd46181bba23402c4e8364f0b616e599986d6
Andrew Guo <andrew.c.guo@gmail.com>
2022-12-04 20:48:08 -0500

    book info

--- pages/books/[isbn].tsx
+++ pages/books/[isbn].tsx
@@ -3,6 +3,9 @@ import { GetServerSidePropsContext } from "next";
 import Layout from "../../components/Layout";
 import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { Database } from "../../utils/database.types";
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import { useState } from "react";
+import { NumericFormat } from "react-number-format";
 type Author = Database["public"]["Tables"]["authors"]["Row"];
 type Publisher = Database["public"]["Tables"]["publishers"]["Row"];
 type Book = Database["public"]["Tables"]["books"]["Row"];
@@ -47,21 +50,97 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   };
 };
 
-const BookSalePage = ({ book, authors, publisher }: { book: Book, authors: Author, publisher: Publisher }) => {
-
+const BookSalePage = ({
+  book,
+  authors,
+  publisher,
+}: {
+  book: Book;
+  authors: any[];
+  publisher: Publisher[];
+}) => {
   const session = useSession();
   const supabase = useSupabaseClient();
+  const [quantity, setQuantity] = useState(0);
 
   return (
-    <Layout title={`${book.title} | Look-Inna-Book`}>
+    <Layout
+      title={
+        !session
+          ? "Login | Look-Inna-Book"
+          : `${book.title} [In-Stock] | Look-Inna-Book`
+      }
+    >
       <div className="flex flex-wrap flex-col items-center justify-around sm:my-14 sm:flex-row sm:items-start sm:justify-center">
         <div className="lg:w-2/5 sm:w-1/2">
           <div className="bg-neutral-800 pt-12 px-12 pb-6 rounded-3xl shadow-xl">
-            <div className="form-widget">
-              <h1 className="text-draculaPink text-3xl pb-4 text-center">
-                {`Title: ${book.title}`}
-              </h1>
-            </div>
+            {!session ? (
+              <div>
+                <h1 className="text-draculaGreen text-3xl pb-4 text-center">
+                  User Login
+                </h1>
+                <Auth
+                  /*providers={["github", "google", "linkedin"]}*/
+                  socialLayout="horizontal"
+                  supabaseClient={supabase}
+                  appearance={{ theme: ThemeSupa }}
+                  magicLink={true}
+                  redirectTo={"${host}"}
+                  theme="dark"
+                />
+              </div>
+            ) : (
+              <div>
+                <h1 className="text-draculaPink text-3xl pb-4 text-center">
+                  {`Title: ${book.title}`}
+                </h1>
+                <div className="text-darkText text-2xl flex flex-col">
+                  <span>${book.msrp}</span>
+                  <span>Number of pages: {book.num_pages}</span>
+                  <span>Authored by:</span>
+                  <ul className="list-disc">
+                    {authors.map((author) => (
+                      <li>{`${author.author_id.first_name} ${author.author_id.middle_name} ${author.author_id.last_name}`}</li>
+                    ))}
+                  </ul>
+                  <span>Published by: </span>
+                  {publisher.map((publisher) => (
+                    <span>
+                      {`${publisher.name} - (${publisher.email})`}
+                    </span>
+                  ))}
+                  <span>Remaining Stock: {book.instock_quantity}</span>
+                </div>
+                <label htmlFor="quantity" className="block mb-6">
+                  <span className="text-darkText">Purchase Quantity:</span>
+                  <NumericFormat
+                    allowNegative={false}
+                    id="quantity"
+                    value={quantity || ""}
+                    onValueChange={(v) => {
+                      setQuantity(Number(v.value));
+                    }}
+                    className="inputField"
+                    placeholder="ex. 10"
+                    decimalScale={0}
+                    isAllowed={(v) => {
+                      return Number(v.value) <= book.instock_quantity ?? 0;
+                    }}
+                    disabled={book.instock_quantity <= 0}
+                  />
+                </label>
+                <div className="mb-6 flex justify-end">
+                  <button
+                    type="submit"
+                    className="saveButton"
+                    onClick={() => {}}
+                    disabled={book.instock_quantity <= 0}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
