84df6765153ffd32d6c58c0ffa121dabe4526d17
Andrew Guo <andrew.c.guo@gmail.com>
2022-12-11 18:16:11 -0500

    made search gui

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,9 @@ const Home = ({ books }: { books: Book[] }) => {
 
       const { data, error } = await supabase
         .from("books")
-        .select("isbn, title, msrp, num_pages, img_url, publisher_id, instock_quantity")
+        .select(
+          "isbn, title, msrp, num_pages, img_url, publisher_id, instock_quantity"
+        )
         .textSearch("title", searchQuery);
 
       if (error) throw error;
@@ -68,8 +70,62 @@ const Home = ({ books }: { books: Book[] }) => {
           {shownBooks.length > 0 ? (
             <div>Welcome!</div>
           ) : (
-            <div>Outta books ¯\_(ツ)_/¯</div>
+            <div>No books found ¯\_(ツ)_/¯</div>
           )}
+        </div>
+        <div className="mt-4">
+          <form className="flex items-center">
+            <label htmlFor="simple-search" className="sr-only">
+              Search
+            </label>
+            <div className="relative w-full">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <input
+                type="text"
+                id="simple-search"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search"
+                value={searchQuery || ""}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => searchForBooks(searchQuery)}
+            >
+              <svg
+                className="w-5 h-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+              <span className="sr-only">Search</span>
+            </button>
+          </form>
         </div>
         <div className="col-span-3 grid lg:grid-cols-3 items-center p-2 m-4 lg:w-4/5 sm:w-2/3">
           {shownBooks.map((book) => (
