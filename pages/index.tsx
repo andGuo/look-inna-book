84ebac7721ac8f14d3a4b97ca5a6a06de16cafce
Andrew Guo <andrew.c.guo@gmail.com>
2022-12-11 18:51:15 -0500

    done full text search

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ const Home = ({ books }: { books: Book[] }) => {
             <div>No books found ¯\_(ツ)_/¯</div>
           )}
         </div>
-        <div className="mt-4">
+        <div className="mt-4 sm:w-1/2">
           <form className="flex items-center" onSubmit={e => { e.preventDefault(); searchForBooks(searchQuery);}}>
             <label htmlFor="simple-search" className="sr-only">
               Search
@@ -107,7 +107,7 @@ const Home = ({ books }: { books: Book[] }) => {
                 type="text"
                 id="simple-search"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search"
+                placeholder="Search Books"
                 value={searchQuery || ""}
                 onChange={(e) => setSearchQuery(e.target.value)}
               />
